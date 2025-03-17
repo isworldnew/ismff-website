2cca7e3b8ab68f8f2b5d6373f4ab13096b967bc9
isworldnew <isworldnew@mail.ru>
2025-03-18 00:09:04 +0400

    multi-page disabling

--- src/Pages/HelloPage/HelloPage.jsx
+++ src/Pages/HelloPage/HelloPage.jsx
@@ -1,14 +1,15 @@
 import './HelloPage.css';
-import Header from '../../CommonComponents/Header/Header'
-import Footer from '../../CommonComponents/Footer/Footer'
+import Header from '../../CommonComponents/Header/Header';
+import Footer from '../../CommonComponents/Footer/Footer';
 
 export default function HelloPage() {
 
     const handleClick = () => {
-        window.location.href="/info-page";
-    }    
+        // window.location.href="/info-page";
+        alert("Работает)");
+    };    
 
-    return(
+    return (
         <>
             <Header />
             <div className="hello-page">
@@ -16,6 +17,11 @@ export default function HelloPage() {
                 <button className="hello-button" onClick={handleClick}>
                     нажми меня
                 </button>
+                <img 
+                    src="../../../public/cat.jpg" 
+                    alt="Пример изображения" 
+                    className="hello-image" 
+                />
             </div>
             <Footer />
         </>
